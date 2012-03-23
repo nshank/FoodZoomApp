ef244750711595b861abf1e3d6309af11de44fc5
Nick Shank <nicholas.shank@gmail.com>
2012-03-23 09:10:08 -0500

    Comined rails - h5bp - zurb foundation to sample map

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodZoomApp::Application.config.secret_token = '4c32d514dc93b37f3bab553dba5cbbf61744e4275f02156e3405be968259a6838fed674c7a125eb07560b8445abc261a280d01ab9b2e9c16eaadeb19f90633aa'
